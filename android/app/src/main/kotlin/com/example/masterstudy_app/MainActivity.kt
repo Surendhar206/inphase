396d745570fac8de8b3552e5b29da4714ef61a4e
Surendhar206 <royalar321@gmail.com>
2022-08-15 15:26:45 +0530

    message

--- android/app/src/main/kotlin/com/example/masterstudy_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/masterstudy_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.learnafrique.course
+package com.education2wealth.courses
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
--- lib/main.dart
+++ lib/main.dart
@@ -314,7 +314,7 @@ class MyAppState extends State<MyApp> {
       create: (BuildContext context) => widget.profileBloc(),
       child: OverlaySupport(
         child: MaterialApp(
-          title: 'Learn Afrique',
+          title: 'Education2wealth',
           theme: _buildShrineTheme(),
           initialRoute: SplashScreen.routeName,
           debugShowCheckedModeBanner: false,
